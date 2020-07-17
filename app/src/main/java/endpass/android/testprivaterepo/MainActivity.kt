01eaa6a2bb73cbdfb492fb937ad8c4985584f953
Meirlen <miko_982@mail.ru>
2020-07-17 11:31:46 +0600

    Initial commit

---
+++ app/src/main/java/endpass/android/testprivaterepo/MainActivity.kt
@@ -0,0 +1,16 @@
+package endpass.android.testprivaterepo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import endpass.android.mylibrary.Connector
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        Connector.testFun()
+
+    }
+}
---
+++ mylibrary/src/main/java/endpass/android/mylibrary/Connector.kt
@@ -0,0 +1,12 @@
+package endpass.android.mylibrary
+
+import endpass.android.endpass_sdk.presentation.router.MainRouter
+
+object Connector {
+
+    val  mainRouter = MainRouter()
+
+    fun testFun(){
+
+    }
+}
